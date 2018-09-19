6e00cb7f9123bcdd1a1c1b813fbb741b4d72d52d
Gihan chanaka <gcbeligaswaththa@gmail.com>
2018-09-20 00:46:32 +0530

    add Constants to Projects-Json Urls

---
+++ CSTechnologieTestProject/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  CSTechnologieTestProject
+//
+//  Created by Gihan Chanaka Beligaswaththa on 9/20/18.
+//  Copyright © 2018 Gihan Chanaka Beligaswaththa. All rights reserved.
+//
+
+import Foundation
+
+let baseUrl = "http://jsonplaceholder.typicode.com/"
+let post = baseUrl + "posts"
+let users = baseUrl + "users"
+let comments = baseUrl + "comments"
